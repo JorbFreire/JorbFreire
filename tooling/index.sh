f6d2f5e5bbc2003ad4ea23f5de7a8d8ca5dda997
JorbFreire <jorbfreire@gmail.com>
2022-11-24 15:43:29 -0300

    add logs

--- tooling/index.sh
+++ tooling/index.sh
@@ -8,7 +8,9 @@ elif [[ $1 == "--help" || $1 == "-h" || $1 == "--h" ]]
   then
     printf "show comands list\n"
     printf "    [new (TITLE*) (PATH)]:  create new option to work on, default PATH is PWD, now would be $PWD\n"
-elif [ $1 == "new" ]
-  printf "Creating new folder option"
-  then newWorkDir "${@:2}"
+elif [[ $1 == "new" ]]
+  then
+    printf "Creating new folder option.\n"
+    newWorkDir "${@:2}"
+else printf "Unkonw option.\n"
 fi
