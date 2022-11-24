1c701973aaf802c5db561402919583e2470ecc2e
JorbFreire <jorbfreire@gmail.com>
2022-11-24 15:37:00 -0300

    improve --help

--- tooling/index.sh
+++ tooling/index.sh
@@ -5,7 +5,10 @@ source $TOOLING_DIR/workdir.sh
 if [[ $# < 1 ]]
   then workdir
 elif [[ $1 == "--help" || $1 == "-h" || $1 == "--h" ]]
-  then echo "show comands list"
+  then
+    printf "show comands list\n"
+    printf "    [new (TITLE*) (PATH)]:  create new option to work on, default PATH is PWD, now would be $PWD\n"
 elif [ $1 == "new" ]
+  printf "Creating new folder option"
   then newWorkDir "${@:2}"
 fi
