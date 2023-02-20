609da25ba92b52a9f2cc6bcda51331f6f8d82bf1
JorbFreire <jorbfreire@gmail.com>
2023-02-20 16:50:09 -0300

    handle node projects

--- tooling/src/main.py
+++ tooling/src/main.py
@@ -1,27 +1,35 @@
 import sys
+from os import getcwd
 from pathlib import Path
+from subprocess import check_call
 
 from actions.select_project import select_project
 from actions.create_project import create_project
 from actions.delete_project import delete_project
 
 project_list_path = Path(__file__).parent / "projects_list.json"
+scripts_path = Path(__file__).parent / "scripts" / "work.sh"
 
-
-if __name__ == "__main__":
+def main():
   if len(sys.argv) < 2:
-    print(select_project(project_list_path))
+    selected_project = select_project(project_list_path)
+    check_call([scripts_path, selected_project["project_path"]])
+    return
 
   option = sys.argv[1]
 
   if option == "create":
-    create_project(project_list_path)
+    return create_project(project_list_path)
 
   if option == "delete":
-    delete_project(project_list_path)
+    return delete_project(project_list_path)
 
   if option == "--help":
     print("avaliable options:") 
     print("  default: run\n")  
     print("  create: create a new project\n") 
     print("  delete: delete a project\n") 
+
+
+if __name__ == "__main__":
+  main()
---
+++ tooling/src/scripts/work.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+source $TOOLING_DIR/src/scripts/try_node.sh
+
+cd "$1"
+
+try_node
