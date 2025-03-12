93a1d1264fcb4f70de15cfd989d4bd2581a70727
Jorb Freire <jorbfreire@gmail.com>
2025-03-12 17:17:21 -0300

    Delete tooling

--- tooling/WORK_PATHS_INDEX.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-declare -A WORK_PATHS_INDEX=(
-  # map list of directories. The key will be the name used on env name of o each item
-  # All paths will be relative do $HOME
-  # Example line:
-  # [NAME]=path/from/home
-)
--- tooling/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-source WORK_PATHS.sh
-
-source newWorkDir.sh
-test
-echo "file 2"
